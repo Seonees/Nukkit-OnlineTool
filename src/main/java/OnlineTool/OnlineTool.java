16710a91a911d7623ed706d91316c773824643e3
Doomhawk <ru.doomhawk@gmail.com>
2016-01-23 13:43:40 +0600

    Обновление 1.1
    - Изменено декодирование URL
    - Запрос производится ассинхронно => сервер не виснет
    - Обновление статуса происходит при измении онлайна, а не циклично

--- src/main/java/OnlineTool/OnlineTool.java
+++ src/main/java/OnlineTool/OnlineTool.java
@@ -4,6 +4,9 @@ import cn.nukkit.Server;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.scheduler.AsyncTask;
 import cn.nukkit.utils.Config;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.event.player.PlayerQuitEvent;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -14,7 +17,6 @@ import java.net.URL;
 import java.net.URLEncoder;
 
 public class OnlineTool extends PluginBase {
-    public int update = 5;
     public String token = "*****";
     public int group = 1;
     public String format = "На сервере сейчас: ONLINE/MAX_PLAYERS игроков!";
@@ -25,43 +27,44 @@ public class OnlineTool extends PluginBase {
         createCfg();
         loadCfg();
         saveCfg();
-        updateStatus();
     }
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e){
+		updateStatus();
+	}
+	
+	@EventHandler
+	public void onQuit(PlayerQuitEvent e){
+		updateStatus();
+	}
 
-    public void upd(){
+    public void updateStatus(){
         if(debug){
             this.getLogger().notice("Отправлено с параметрами: group_id="+group+", text="+format.replace("ONLINE",String.valueOf(this.getServer().getOnlinePlayers().size())).replace("MAX_PLAYERS",String.valueOf(this.getServer().getMaxPlayers())));
         }
         sendGet("https://api.vk.com/method/status.set?group_id="+group+"&text="+format.replace("ONLINE",String.valueOf(this.getServer().getOnlinePlayers().size())).replace("MAX_PLAYERS",String.valueOf(this.getServer().getMaxPlayers())).replace(" ","%20")+"&access_token="+token);
     }
 
-    public void updateStatus(){
-        this.getServer().getScheduler().scheduleDelayedRepeatingTask(        new Runnable() {
-            public void run() {
-                upd();
-            }
-        },20*update,20*update);
-    }
-
     private void sendGet(final String url) {
         Server.getInstance().getScheduler().scheduleAsyncTask(new AsyncTask() {
-            @Override
-            public void onRun() {
-                try {
-                    URL obj = new URL(URLEncoder.encode(url,"UTF-8"));
-                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-                    con.setRequestMethod("GET");
-                    con.setRequestProperty("User-Agent", "Mozilla/5.0");
-                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                    String inputLine;
-                    StringBuffer response = new StringBuffer();
-                    while ((inputLine = in.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-                    in.close();
-                } catch(Exception e){}
-            }
-        });
+				@Override
+				public void onRun() {
+					try {
+						URL obj = new URL(URLEncoder.encode(url,"UTF-8"));
+						HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+						con.setRequestMethod("GET");
+						con.setRequestProperty("User-Agent", "Mozilla/5.0");
+						BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+						String inputLine;
+						StringBuffer response = new StringBuffer();
+						while ((inputLine = in.readLine()) != null) {
+							response.append(inputLine);
+						}
+						in.close();
+					} catch(Exception e){}
+				}
+			});
 
     }
 
@@ -77,7 +80,6 @@ public class OnlineTool extends PluginBase {
 
     private void loadCfg(){
         Config cfg = this.getConfig();
-        update = cfg.getNested("onlinetool.update",5);
         token = cfg.getNested("onlinetool.token","*****");
         group = cfg.getNested("onlinetool.group",1);
         format = cfg.getNested("onlinetool.format","На сервере сейчас: ONLINE/MAX_PLAYERS игроков!");
@@ -92,7 +94,6 @@ public class OnlineTool extends PluginBase {
 
     private void saveCfg(){
         Config cfg = this.getConfig();
-        cfg.setNested("onlinetool.update",update);
         cfg.setNested("onlinetool.token",token);
         cfg.setNested("onlinetool.group",group);
         cfg.setNested("onlinetool.format",format);
